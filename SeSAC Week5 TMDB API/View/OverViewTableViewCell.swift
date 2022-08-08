ed317ff5395d321c834c6ecc37f14ffb3b9a446f
roargenie <105975078+roargenie@users.noreply.github.com>
2022-08-08 21:20:22 +0900

    ✅TMDB API를 활용한 영화정보 표현

--- SeSAC Week5 TMDB API/View/OverViewTableViewCell.swift
+++ SeSAC Week5 TMDB API/View/OverViewTableViewCell.swift
@@ -18,13 +18,16 @@ class OverViewTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        
+        
     }
 
 }
